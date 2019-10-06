cfa86a5134572f13283a2bb63a3caff161f45e40
Enrique Ortiz <oice6703@gmail.com>
2019-10-05 21:53:48 -0500

    First round

---
+++ lib/screens/task_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
