e77ff6ca1c864fe72d299164494b8a3a2fa813a7
Enrique Ortiz <oice6703@gmail.com>
2019-10-10 14:44:22 -0500

    State challenge start

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,27 @@
+import 'dart:ui' as prefix0;
+
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
+
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration:
+            isChecked ? TextDecoration.lineThrough : TextDecoration.none),
+      ),
       trailing: Checkbox(
-        value: false,
-        onChanged: null,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
       ),
     );
   }
